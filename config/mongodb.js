ef5d287519a30a3208e9b594aab9530809902bd3
Bhavya Dalal <bhavyadalal@192.168.1.10>
2025-01-12 15:41:25 +0530

    final updated code with razor pay

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config()
 const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => {
-            console.log("DB Connected");
-        });
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("DB Connected");
+    });
 
-        // Use the URI from the environment variable
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+    // Use the URI from the environment variable
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log("MongoDB connection successful.");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error.message);
-        process.exit(1); // Exit the process with failure
-    }
+    console.log("MongoDB connection successful.");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit the process with failure
+  }
 };
 
-export default connectDB;
+ export default connectDB;
